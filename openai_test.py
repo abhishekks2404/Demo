d79828905321f131180bf35434e2033343a53f48
Abhishek Kumar Singh <139315513+abhishekks2404@users.noreply.github.com>
2024-04-12 16:53:42 +0530

    Update openai_test.py

--- openai_test.py
+++ openai_test.py
@@ -10,9 +10,9 @@ def get_query(Prompt):
 
   
     client = AzureOpenAI(
-                azure_endpoint = os.getenv("azure_endpoint"),
-                api_key = os.getenv("api_key"),
-                api_version = os.getenv("api_version")
+                azure_endpoint = st.secrets["azure_endpoint"],
+                api_key = st.secrets["api_key"],
+                api_version = st.secrets["api_version"]
             )
     conversation1 = [{"role": "system", "content": f"""You are a assistant, just reply with details answer.
     """},
